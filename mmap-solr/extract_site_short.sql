c3fab7f6b30cff7ad84aff439c84bb9c12a5e915
John B. Lowe <jblowe@berkeley.edu>
2025-12-02 15:58:48 -0800

    tidy up the mmap-solr directory

--- mmap-solr/extract_site_short.sql
+++
@@ -1,1 +0,0 @@
-select site_name,site_short,siteid,count(*)  from tblsite group by site_name,site_short,siteid ;
--- mmap-solr/solrETL-site-photos.sh
+++
@@ -1,8 +0,0 @@
-find ~/image_repos/LaosPhotos/derivatives -type f | grep -v DS_Store > mmap-sites.txt
-perl -ne 'chomp ; s#/Users/johnlowe/image_repos/LaosPhotos/derivatives/##;print;print "\t";s#/#\t#g;s/ /\t/;print "$_\n"' mmap-sites.txt > sites1.tmp
-python3 evaluate.py sites1.tmp sites2.tmp
-cat mmap-sites.header sites2.tmp > mmap-sites.csv
-perl -pe 's/\t/\n/g' mmap-sites.header > mmap-sites.fields.txt
-./makesolrcores.sh mmap-sites
-./solrETL-sites.sh mmap
-./sitesETL.sh mmap-sites.csv mmap-sites
