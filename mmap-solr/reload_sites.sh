070a301ebef3acdf1d71621f41fe83710116706f
John B. Lowe <jblowe@berkeley.edu>
2026-02-01 00:27:12 -0800

    revise reload-sties.sh to be more tolerant of invalid photo type directories

--- mmap-solr/reload_sites.sh
+++ mmap-solr/reload_sites.sh
@@ -11,7 +11,9 @@ grep site_name mmap-dbsites.csv | perl -pe 's/ +/_/g' > mmap-dbsites.header.csv
 
 # make a list of the site images and prep it for solr
 find "${DERIVATIVES}" -type f | grep -v DS_Store | grep -v extra_maps > sites0.tmp
-perl -ne 'chomp ;
+perl -ne '
+next if /Old-/i;
+chomp ;
 s#^.*?/derivatives/##;
 print;print "\t";
 s/Articts/Artifacts/;
@@ -21,14 +23,19 @@ s#/#\t#g;
 # only do this once per line to insert a tab between data and site name
 s/ /\t/;
 s/\t\d+\. /\t/;
-s#Artifacts\tStudio Individual +Shot Art#Studio_artifact_shot#i;
-s#Artifacts\tStudio Bag Shot Artifact#Studio_bag_shot#i;
-s#Artifacts\tStudio Bag Shot Art#Studio_bag_shot#i;
-s#Artifacts\tIn situ Artifacts?#Artifacts_on_site#i;
+s#(Artifacts\t)?Studio Individual +Shot Art#Studio_artifact_shot#i;
+s#(Artifacts\t)?Studio Bag Shot Artifact#Studio_bag_shot#i;
+s#(Artifacts\t)?Studio Bag Shot Art#Studio_bag_shot#i;
+s#(Artifacts\t)?Studio Bag shot#Artifacts_bag_shot#i;
+s#(Artifacts\t)?In situ Artifacts?#Artifacts_on_site#i;
+s#Artifacts +on site#Artifacts_on_site#i;
+s#Artifacts in situ#Artifacts_on_site#i;
+# skip directories whose resolved names contain blanks
 print "$_\n";' \
 sites0.tmp > sites1.tmp
+perl -ne '@x=split/\t/;print if (@x[3] =~ / /)' sites1.tmp | cut -f1 | perl -pe 's#(.*)\/.*#\1#' | uniq
 
-perl -ne 'print if /\.(jpg|jpeg|tif|gif|png|webp)/i' sites1.tmp > sites2.tmp
+perl -ne '@x=split/\t/;next if @x[3] =~ /( |JPG)/; print if /\.(jpg|jpeg|tif|gif|png|webp)/i' sites1.tmp > sites2.tmp
 cat mmap-site-photos.header.csv sites2.tmp > mmap-site-photos.csv
 perl -pe 's/\t/\n/g;s/\r//g;' mmap-site-photos.header.csv > mmap-site-photos.fields.txt
 python3 evaluate.py mmap-site-photos.csv sites3.tmp
